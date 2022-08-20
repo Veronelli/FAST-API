2a4d40eb29b20658e18450fa437d259b2020e9a8
Veronello <facu2000veronelli@outlook.com>
2022-08-20 16:57:47 -0300

    finish class

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+# Models
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+
+    hair_color: Optional[str]
+    is_married: Optional[bool]
 
 @app.get("/")
 def home():
     return {"message":"Hello World"}
+
+# Request and Response Body
+@app.post("/person/new")
+def create_person(person:Person = Body(...)):
+    return person
+
